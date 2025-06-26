319db62f21d00222bdc3bab16990bfbb69b6a6b7
Your Name <your@email.com>
2025-06-26 17:02:41 +0530

    Some updates on Ui

--- frontend/src/steps/SuccessStep.jsx
+++ frontend/src/steps/SuccessStep.jsx
@@ -1,22 +1,3 @@
-// import React from 'react';
-// import { Typography, Button } from '@mui/material';
-// import { useDispatch } from 'react-redux';
-// import { resetForm, setStep } from '../features/feedbackSlice';
-
-// export default function SuccessStep() {
-//   const dispatch = useDispatch();
-//   const handleNew = () => {
-//     dispatch(resetForm());
-//     dispatch(setStep(0));
-//   };
-//   return (
-//     <div style={{ textAlign: 'center', padding: 32 }}>
-//       <Typography variant="h5" gutterBottom>Thank you for your feedback!</Typography>
-//       <Button variant="contained" onClick={handleNew}>Submit Another Feedback</Button>
-//     </div>
-//   );
-// } 
-
 import React from 'react';
 import { Typography, Button } from '@mui/material';
 import { useDispatch, useSelector } from 'react-redux';
